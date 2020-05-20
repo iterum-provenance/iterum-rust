46ef3f5e94896682915b968cd6ba057259320604
Mark <marktmmrmn@gmail.com>
2020-05-20 14:38:20 +0200

    Added utils module

---
+++ src/utils.rs
@@ -0,0 +1,13 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use std::iter;
+
+pub fn create_random_hash() -> String {
+    let mut rng = thread_rng();
+    let hash: String = iter::repeat(())
+        .map(|()| rng.sample(Alphanumeric))
+        .take(32)
+        .collect();
+
+    hash
+}
