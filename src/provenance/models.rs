f39236b46c815cf8792561034cf681a1ec2864a1
Mark <marktmmrmn@gmail.com>
2020-07-22 16:19:23 +0200

    Added default for custom field

--- src/provenance/models.rs
+++ src/provenance/models.rs
@@ -1,10 +1,15 @@
 use serde::{Deserialize, Serialize};
 use serde_json::value::Value;
 
+pub fn empty_value() -> Value {
+    Value::Null
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Metadata {
     pub fragment_id: String,
     pub predecessors: Vec<String>,
+    #[serde(default = "empty_value")]
     pub custom: Value,
 }
 
