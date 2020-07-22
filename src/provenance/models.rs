ef6469a89fa0904a3c72849a422d82e72717639d
Mark <marktmmrmn@gmail.com>
2020-07-22 14:39:05 +0200

    Update to fragment lineage struct

--- src/provenance/models.rs
+++ src/provenance/models.rs
@@ -1,7 +1,28 @@
 use serde::{Deserialize, Serialize};
+use serde_json::value::Value;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Metadata {
+    pub fragment_id: String,
+    pub predecessors: Vec<String>,
+    pub custom: Value,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct FileDescription {
+    pub name: String,
+    pub path: String,
+    pub bucket: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct FragmentDescription {
+    pub metadata: Metadata,
+    pub files: Vec<FileDescription>,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct FragmentLineage {
     pub transformation_step: String,
-    pub predecessors: Vec<String>,
+    pub description: FragmentDescription,
 }
